a83e52dfe996c1b4a311bc02dc11b13fe5595e10
Polina Shumilina <Shumilina246@gmail.com>
2022-09-16 14:43:40 +0300

    add: routers & controllers

--- server/index.js
+++ server/index.js
@@ -1,10 +1,20 @@
 require("dotenv").config();
 const express = require("express");
+const models = require("./models/models");
 const sequelize = require("./db");
+const cors = require("cors");
+const router = require("./routes/index");
+const errorHandler = require('./middleware/ErrorHandlingMiddleware')
 
 const PORT = process.env.PORT;
 
 const app = express();
+app.use(cors());
+app.use(express.json());
+app.use("/api", router);
+
+//Обработка ошибок - последний middleware
+app.use(errorHandler)
 
 const start = async () => {
   try {

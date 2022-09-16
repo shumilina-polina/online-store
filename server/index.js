bb5d347d87860f59481ae8b582930cc170c99149
Polina Shumilina <Shumilina246@gmail.com>
2022-09-16 16:14:48 +0300

    create: base api

--- server/index.js
+++ server/index.js
@@ -1,20 +1,24 @@
 require("dotenv").config();
 const express = require("express");
+const fileUpload = require("express-fileupload");
 const models = require("./models/models");
 const sequelize = require("./db");
 const cors = require("cors");
+const path = require("path");
 const router = require("./routes/index");
-const errorHandler = require('./middleware/ErrorHandlingMiddleware')
+const errorHandler = require("./middleware/ErrorHandlingMiddleware");
 
 const PORT = process.env.PORT;
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.resolve(__dirname, "static")));
+app.use(fileUpload({}));
 app.use("/api", router);
 
 //Обработка ошибок - последний middleware
-app.use(errorHandler)
+app.use(errorHandler);
 
 const start = async () => {
   try {
